52db9551bdd019841e7048bb9718eb7e15a429bb
jryxxx <1499948365@qq.com>
2024-05-08 15:09:12 +0800

    initial commit

---
+++ build/intermediates/host/CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/thtf/samples/inference/modelInference/sampleYOLOV7/out/main"
+  "/home/thtf/samples/inference/modelInference/sampleYOLOV7/out/main.pdb"
+  "CMakeFiles/main.dir/sampleYOLOV7.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
